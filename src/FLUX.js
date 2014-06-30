73067fd197d39d8c2597e278768e2d2ad61b1d0c
Joseph Clay <testingriven@gmail.com>
2014-06-29 22:37:07 -0500

    Add a noConflict method

--- src/FLUX.js
+++ src/FLUX.js
@@ -1,7 +1,16 @@
-module.exports = window.FLUX = {
+var prevFlux = window.FLUX;
+
+var FLUX = {
 	Tween: require('./animators/FluxTween'),
 	Spring: require('./animators/FluxSpring'),
 	
 	Easing: require('./Easing'),
-	update: require('./loop').update
+	update: require('./loop').update,
+
+	noConflict: function() {
+		window.FLUX = prevFlux;
+		return FLUX;
+	}
 };
+
+module.exports = window.FLUX = FLUX;
